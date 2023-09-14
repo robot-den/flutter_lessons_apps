54c03ea9263c0cab5014ff4c02c914c872e248af
Denis Nazmutdinov <w.nazmutdinov@gmail.com>
2023-09-15 00:26:25 +0300

    recipes app: lessons 1 - 4 - explicit animations

--- recipes/lib/features/categories/view/categories_list_screen.dart
+++ recipes/lib/features/categories/view/categories_list_screen.dart
@@ -4,28 +4,65 @@ import 'package:get_it/get_it.dart';
 import 'package:recipes/features/categories/widgets/widgets.dart';
 import 'package:recipes/repository/category/abstract_categories_repository.dart';
 
-class CategoriesListScreen extends StatelessWidget {
+class CategoriesListScreen extends StatefulWidget {
   const CategoriesListScreen({super.key});
+
+  @override
+  State<CategoriesListScreen> createState() => _CategoriesListScreenState();
+}
+
+class _CategoriesListScreenState extends State<CategoriesListScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      // lowerBound: 0,
+      // upperBound: 1,
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final categoriesRepository = GetIt.I<AbstractCategoriesRepository>();
     final categories = categoriesRepository.categories();
 
-    return GridView(
-      padding: const EdgeInsets.all(16),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-      ),
-      children: [
-        for (final category in categories)
-          CategoryGridItem(
-            category: category,
-          )
-      ],
-    );
+    return AnimatedBuilder(
+        animation: _animationController,
+        child: GridView(
+          padding: const EdgeInsets.all(16),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          children: [
+            for (final category in categories)
+              CategoryGridItem(category: category)
+          ],
+        ),
+        builder: (context, child) {
+          return Padding(
+            padding: EdgeInsets.only(
+              top: 100 - _animationController.value * 100,
+            ),
+            child: child,
+          );
+        });
   }
 }
